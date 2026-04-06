1d3a87de44374c2872cc6809eb2956fdcead0e4b
EvezArt <evezart@github.com>
2026-04-06 12:45:03 +0000

    EVEZ-OS v0.1: Sensory Engine, skills, outreach

---
+++ evez-os/core/modules/__init__.py
@@ -0,0 +1,18 @@
+# EVEZ-OS Modules
+from .trunk_manager import TrunkManager
+from .branch_executor import BranchExecutor
+from .ledger import EvezLedger
+from .skeptic_entity import SkepticEntity
+from .child_entity import ChildEntity
+from .provenance import ProvenanceTracker
+from .psyop_engine import PsyopEngine
+
+__all__ = [
+    "TrunkManager",
+    "BranchExecutor", 
+    "EvezLedger",
+    "SkepticEntity",
+    "ChildEntity",
+    "ProvenanceTracker",
+    "PsyopEngine"
+]
